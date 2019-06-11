e8076232da56b4a38a97817bbd455542d0e230f9
Jonathan Moore <jmjonemoore@gmail.com>
2019-06-11 15:51:36 -0500

    Completed Authorization

--- server.js
+++ server.js
@@ -11,8 +11,16 @@ const app = express()
 app.use(morgan('dev'))
 
 app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get('Authorization')
+  console.log(apiToken)
+  console.log(authToken.split(' ')[1])
+
+  if(!authToken || authToken.split(' ')[1] !== apiToken) {
+    return res.status(401).json({error: 'Unauthorized request'})
+  }
+  
   console.log('validate bearer token middleware')
-  debugger
   // move to the next middleware
   next()
 })
