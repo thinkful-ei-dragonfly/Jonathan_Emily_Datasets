0fc2be55a3954918a360c1f89fc3a5627b399979
Jonathan Moore <jmjonemoore@gmail.com>
2019-06-23 23:42:05 -0500

    Changes before deploying

--- server.js
+++ server.js
@@ -3,24 +3,22 @@ require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 
-console.log(process.env.API_TOKEN)
 
 
 const app = express()
 
-app.use(morgan('dev'))
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+app.use(morgan(morganSetting))
 
 app.use(function validateBearerToken(req, res, next) {
   const apiToken = process.env.API_TOKEN;
   const authToken = req.get('Authorization')
-  console.log(apiToken)
-  console.log(authToken.split(' ')[1])
-
-  if(!authToken || authToken.split(' ')[1] !== apiToken) {
-    return res.status(401).json({error: 'Unauthorized request'})
-  }
   
-  console.log('validate bearer token middleware')
+
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    return res.status(401).json({ error: 'Unauthorized request' })
+  }
+
   // move to the next middleware
   next()
 })
@@ -40,10 +38,18 @@ function handleGetPokemon(req, res) {
 
 app.get('/pokemon', handleGetPokemon)
 
+app.use((error, req, res, next) => {
+  let response
+  if (process.env.NODE_ENV === 'production') {
+    response = { error: { message: 'server error' }}
+  } else {
+    response = { error }
+  }
+  res.status(500).json(response)
+})
 
-const PORT = 8000
+const PORT = process.env.PORT || 8000
 
 app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`)
 })
 
